79343350cf54ed2428f5c2a318573e80d690ff1c
JuanJGimenez <mdqgimenez@gmail.com>
2022-03-25 18:05:01 -0300

    subo los archivos

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
+
+// app.listen(3000, () => console.log("Servidor corriendo en el puerto 3000"));
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Servidor corriendo en el puerto 3000");
+})
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/home.html"));
+});
+
+// aqui con path.resolve
+app.get("/home", (req, res) => {
+    res.sendFile(path.resolve("./views/home.html"))
+});
+
+app.get("/views/register", (req, res) => {
+    res.sendFile(path.resolve("./views/register.html"))
+});
+
+app.get("/views/login", (req, res) => {
+    res.sendFile(path.resolve("./views/login.html"))
+});
